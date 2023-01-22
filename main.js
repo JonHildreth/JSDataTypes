53a9d581810682d6afc4977e58f84d2afb7f2989
JonHildreth <JonathanHildreth0@gmail.com>
2023-01-22 18:06:53 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,83 @@
+//---JavaScript Practice---//
+
+//String - for storing and manipulating text//
+
+let firstName = 'Shakita'
+let lastName = 'Savage'
+let carColor = 'Black'
+let CPU = 'Ryzen'
+let OS = 'Linux'
+let distro = 'Ubuntu'
+let meat = 'Beef'
+let beef = 'Steak'
+let steak = 'Medium-Rare'
+let side = 'Mashed Potatoes'
+
+//Integer//
+
+let a = 1
+let b = 2
+let c = 3
+let d = 4
+let e = 5
+let f = 6
+let g = 7
+let h = 8 
+let i = 9
+let j = 10
+
+//Array - variables that hold multiple values//
+
+const TDE = ['Kendrick', 'ScHoolBoyQ', 'Ab-Soul', 'JayRock', 'Isaiah Rashad', 'SZA'];
+const presidents = ['Kennedy', 'Obama', 'Lincoln']
+const cities = ['New York', 'Philadelphia', 'Detroit', 'Chicago', 'Newark']
+const boroughs = ['Brooklyn', 'Manhattan', 'Bronx', 'Staten Island', 'Queens']
+const bxhoods = ['Tremont', 'Fordham', 'Concourse']
+const philly = ['West Philadelphia', 'South Philadelphia', 'Olney','Kensington',]
+const detroit = ['Hamtramck', 'Brightmoor', 'Belmont']
+const metrodetroit = ['Warren', 'Pontiac', 'Farmington Hills', 'RedFord', 'Sterling Heights']
+const dStreets = ['McNichols', '8Mile', 'Fenkell', 'John R.', 'State Fair']
+const languages = ['C++', 'Python', 'SQL', 'C#', 'VisualBasic']
+
+//Object//
+
+const dog = {
+    firstName: 'Momma',
+    lastName: 'Mia',
+};
+const cat = {
+    firstName: 'street',
+    lastName: 'cat',
+};
+const car = {
+    make: 'Ford',
+    model: 'Taurus',
+};
+const couch = {
+    width: 'loveseat',
+    material: 'Leather',
+};
+const hoodie = {
+    color: 'Black',
+    size: 'large',
+};
+const sister = {
+    firstName: 'Sydney',
+    lastName: 'Hildreth',
+};
+const brother = {
+    firstName: 'Brandon',
+    lastName: 'Perkins',
+};
+const phone = {
+    OS: 'Android',
+    manufacturer: 'Samsung',
+};
+const Piano = {
+    length: '88Key',
+    manufacturer: 'Yamaha'
+}
+const Bed = {
+    size: 'Queen',
+    material: 'foam', 
+};
